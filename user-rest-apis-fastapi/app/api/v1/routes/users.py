822d551db207cb794c8dde870a8142ef9c572e5d
Arshdeep <kaurarshdeep0103@gmail.com>
2026-06-24 01:03:00 +0530

    Basic CRUD FastAPI application

--- user-rest-apis-fastapi/app/api/v1/routes/users.py
+++ user-rest-apis-fastapi/app/api/v1/routes/users.py
@@ -1,7 +1,30 @@
 from fastapi import APIRouter
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/users",
+    tags=["Users"]
+)
+
+users = []
 
 @router.get("/")
 def get_users():
     return {"message": "All users"}
+
+
+@router.get("/{student_id}")
+def get_user(student_id:int):
+    return {"message": f"Details of student with student id : {student_id}"}
+
+@router.post("/")
+def create_user():
+    return {"message": "user with is created"}
+
+@router.put("/{student_id}")
+def update_user(student_id:int):
+    return {"message": f"Student details with student id {student_id} is updated"}
+
+@router.delete("/{student_id}")
+def delete_user(student_id:int):
+    return {"message": f"Student with student id {student_id} is deleted"}
+
--- user-rest-apis-fastapi/main.py
+++ user-rest-apis-fastapi/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 
-from api.v1.routes import router as user_router
+from app.api.v1.routes.users import router as user_router
 
 app =  FastAPI()
 
 app.include_router(
     user_router,
-    prefix="/users"
+    prefix="/api/v1"
 )
